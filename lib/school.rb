f675227e7f0ee2acb98483bd7f8c25c1aec3578a
JazzPants <leighton.l14@gmail.com>
2022-01-20 10:56:12 +1100

    Donegit add .

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name=name
+        @roster= {}
+    end
+
+    def add_student(student, grade)
+            if roster[grade] == nil
+              roster[grade] = []
+            end
+            roster[grade] << student
+        end
+
+    def grade(number)
+        roster[number]
+    end
+
+    def sort
+        sorted = {}
+        roster.each do |grade, students| 
+            sorted[grade] = students.sort
+    end
+        sorted
+    end
+
+end
+
+school = School.new("Bayside High School")
+school.add_student("Zach Morris", 9)
+school.roster
+# => {9 => ["Zach Morris"]}
